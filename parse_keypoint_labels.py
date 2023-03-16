db2ce7ddbbf2adf535f52fe3a6c9190a001a8340
Antoine Duchesne <antduchesne@hotmail.fr>
2023-03-16 14:19:03 +0100

    modif script keypoints, plus de []

--- parse_keypoint_labels.py
+++ parse_keypoint_labels.py
@@ -2,16 +2,16 @@ import os
 from os import listdir
 
 f = open('keypoints_labels_data.csv','w')
-folder_dir = "C:/Users/antdu/Mon Drive/projet/data/keypoints_labels"
+folder_dir = "G:/Mon Drive/projet/data/keypoints_labels"
 for file in os.listdir(folder_dir):
-    ffile = "C:/Users/antdu/Mon Drive/projet/data/keypoints_labels/" + file
+    ffile = "G:/Mon Drive/projet/data/keypoints_labels/" + file
     f1 = open(ffile,'r')
     for line in f1:
         unformated_line = line.replace(", "," ")
         formated_line = "{}, {}".format(file,unformated_line)
         short_formated_line = formated_line[:len(formated_line)-3]
-        short_usable_line = short_formated_line.replace("]","],")
-        usable_line = short_usable_line+"]]"
+        short_usable_line = short_formated_line.replace("]",",")
+        usable_line = short_usable_line.replace("[","")
         f.write(usable_line+'\n')
 
 f.close()
