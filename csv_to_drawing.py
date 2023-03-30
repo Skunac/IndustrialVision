fbbfb48614ab2fe07c05c6ab094a7d706c64a131
Amine KHEDDAOUI <61601575+AmineKheddaoui@users.noreply.github.com>
2023-03-30 15:34:08 +0200

    Add files via upload

--- csv_to_drawing.py
+++ csv_to_drawing.py
@@ -1,16 +1,17 @@
-import numpy
 import matplotlib.pyplot as plt
 import pandas as pd
 import csv
 import plotly.express as px
 import plotly.graph_objects as go
-
+import plotly.subplots as sp
 
 df = pd.read_csv(
-    'test.csv', usecols=['a','b','c'], sep=",")
-fig = px.scatter_3d(df, x="a",y="b",z="c")
+    'test.csv', usecols=['a','b','c','d','e','f'], sep=",")
+fig1 = px.scatter_3d(df, x="a", y="b", z="c", color='d')
+plt.hold()
+fig1 = px.line_3d(df, x="a", y="b", z="c",color='d')
 
-fig.update_layout(scene=dict(
+fig1.update_layout(scene=dict(
     xaxis_title='X AXIS',
     yaxis_title='Y AXIS',
     zaxis_title='Z AXIS'),
@@ -18,5 +19,4 @@ fig.update_layout(scene=dict(
     margin=dict(r=20, b=10, l=10, t=10))
 
 
-
-fig.show()
+fig1.show()
