b651fb73ad667079ff2e4778f3ae3ddd0fe6087b
malokorn <amine.kheddaoui@gmaiL.com>
2023-04-05 17:43:25 +0200

    modifié :         cluster.py

--- cluster.py
+++ cluster.py
@@ -5,15 +5,18 @@ import pandas as pd
 import os
 from os import listdir
 
-def squareToPoint(x1,x2,y1,y2):
-    midX =  (x1+x2)/2
-    midY =  (y1+y2)/2
-    return (midX, midY)
+def middlePoint(x1,x2):
+    mid =  (x1+x2)/2
+    return (mid)
 
 df = pd.read_csv(
     'data_link_output_img.csv', usecols=['vx11','vx12','vy11','vy12'], sep=",")
 
-middle = []
+middleX = []
+middleY = []
 for row in df:
     for i in range(1, 50):
-        middle[0:2*i] = squareToPoint(df.vx11, df.vx12, df.vy11, df.vy12) #Le 0:2*i c'est parce qu'on retourne 2 valeurs à chaque fois dans squareToPoint()
+        middleX[i] = middlePoint(df.vx11, df.vx12)
+        middleY[i] = middlePoint(df.vy11, df.vy12)
+
+print(middleX)
