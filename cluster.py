ab051650b69402b81ef309413f18032bfc87853e
malokorn <amine.kheddaoui@gmaiL.com>
2023-04-05 17:48:59 +0200

    modifié :         cluster.py

--- cluster.py
+++ cluster.py
@@ -1,22 +1,19 @@
-import numpy
-import matplotlib.pyplot as plt
-import csv
 import pandas as pd
-import os
-from os import listdir
+import matplotlib.pyplot as plt
 
-def middlePoint(x1,x2):
-    mid =  (x1+x2)/2
-    return (mid)
 
-df = pd.read_csv(
-    'data_link_output_img.csv', usecols=['vx11','vx12','vy11','vy12'], sep=",")
+def middlePoint(x1, x2):
+    mid = (x1 + x2) / 2
+    return mid
 
-middleX = []
-middleY = []
-for row in df:
-    for i in range(1, 50):
-        middleX[i] = middlePoint(df.vx11, df.vx12)
-        middleY[i] = middlePoint(df.vy11, df.vy12)
+
+df = pd.read_csv('data_link_output_img.csv', usecols=[
+                 'vx11', 'vx12', 'vy11', 'vy12'], sep=',')
+
+middleX = [0] * 50
+middleY = [0] * 50
+for i, row in df.iterrows():
+    middleX[i] = middlePoint(row['vx11'], row['vx12'])
+    middleY[i] = middlePoint(row['vy11'], row['vy12'])
 
 print(middleX)
