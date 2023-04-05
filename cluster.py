c08205fdf34b5c237585189d552af798962a256c
malokorn <amine.kheddaoui@gmaiL.com>
2023-04-05 17:33:57 +0200

    modifié :         cluster.py

--- cluster.py
+++ cluster.py
@@ -11,10 +11,9 @@ def squareToPoint(x1,x2,y1,y2):
     return (midX, midY)
 
 df = pd.read_csv(
-    'IndustrialVision/data_link_output_img.csv', usecols=['vx11','vx12','vy11','vy12'], sep=",")
+    'data_link_output_img.csv', usecols=['vx11','vx12','vy11','vy12'], sep=",")
 
 middle = []
 for row in df:
-    for i in range(1, 20):
-        middle[i] = squareToPoint(df.vx11, df.vx12, df.vy11, df.vy12)
-
+    for i in range(1, 50):
+        middle[0:2*i] = squareToPoint(df.vx11, df.vx12, df.vy11, df.vy12) #Le 0:2*i c'est parce qu'on retourne 2 valeurs à chaque fois dans squareToPoint()
